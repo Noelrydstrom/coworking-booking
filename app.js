7f8df170393fe22d45116a4256d527f382e65cc0
Noel Rydstrom <ironnoel33@gmail.com>
2026-04-07 18:06:48 +0200

    Final touches for the application on render

--- app.js
+++ app.js
@@ -7,11 +7,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("API is running 🚀");S
+});
+
 // Routes
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/rooms", require("./routes/roomRoutes"));
 app.use("/api/bookings", require("./routes/bookingRoutes"));
 
-module.exports = app;
-
 app.use(errorHandler);
+module.exports = app;
